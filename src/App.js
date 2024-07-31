b0f79e4ac5e1f68fb85f31334461f3cc040fc702
mamadusama <seu_email@example.commamadusamadev@gmail.com>
2024-07-31 18:12:07 +0100

    feat: add new lesson

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState(""); // criar um stado para nome
+  const [email, setEmail] = useState("");//criar um stado para email
+  const [age, setAge] = useState("");// criar um stado par idade
+  const [user, setUser] = useState({});//estado para objeto
+
+  function handelCadastrar(e) {
+    e.preventDefault(); // previne para que a pagina não atualize automaticamente
+
+    setUser({
+      name: name,
+      email: email,
+      age: age,
+    });
+
+    // esvaziar campos  depois de submeter form
+    setName("");
+    setEmail("");
+    setAge("");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>formulário de submição</h1>
+        <form onSubmit={handelCadastrar}>
+          <label>Nome</label>
+          <br></br>
+          <input
+            placeholder="diite seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br></br>
+
+          <label>Email</label>
+          <br></br>
+          <input
+            placeholder="diite seu Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br></br>
+
+          <label>Idade</label>
+          <br></br>
+          <input
+            placeholder="diite sua Idade"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <br></br>
+
+          <br></br>
+          <button type="submit">cadastrar</button>
+        </form>
+        <br></br>
+
+        <div>
+          <span>
+            Nome: {user.name}
+            <br></br>
+            Email: {user.email}
+            <br></br>
+            Idade: {user.age}
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Formulario.js
@@ -0,0 +1,10 @@
+
+function Formulario ({nome, idade, cidade}){
+    return (
+      <span>
+        Nome: {nome } <br></br>Idade: {idade}  <br></br> Cidade: {cidade}
+      </span>
+    );
+}
+ 
+export default Formulario;
